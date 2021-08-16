8466474af61042cd525d2fba8d41aaf986fac1c0
jessijoke <jessijoke@yahoo.com>
2021-08-16 00:16:33 -0400

    passing list as props, added grid

--- src/App.js
+++ src/App.js
@@ -1,38 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'm1'
-        },
-        {
-          name: 'Dracula',
-          id: 'm2'
-        },
-        {
-          name: 'Zombie',
-          id: 'm3'
-        }
-      ]
+      monsters: []
     };
   }
 
-  
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(resp => resp.json())
+    .then(users => this.setState({monsters: users}))
+  }
 
   render() {
     return (
       <div className="App">
+      <CardList>
         {
           this.state.monsters.map(monster => (
             <h1 key={monster.id}> {monster.name} </h1>
             ))
         }
+      </CardList>
+       
       </div>
     );
   }
